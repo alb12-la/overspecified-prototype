2e0de9ceef5db77314239a5ab1379cdc68f4be1e
alb12-la <alb12@ucla.edu>
2021-01-27 16:50:07 -0800

    cards + login fix

--- src/app/components/analytics-list/analytics-list.component.ts
+++ src/app/components/analytics-list/analytics-list.component.ts
@@ -9,102 +9,23 @@ import { InterviewService, QuestionTypeEnum } from 'src/app/services/interview.s
 export class AnalyticsListComponent implements OnInit {
   constructor(private interviewService: InterviewService) { }
   chartData = [];
+
+  // TODO REMOVE MOCK BELOW
   data = [
-    {
-      title: {
-        text: 'Question-01',
-        subtext: 'Mocking Data',
-        x: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      calculable: true,
-      series: [
+    this.chartGenerator({
+      question: 'When you come to a fork in the road, and there are no options left, which way do you go?',
+      questionType: 'multiplechoice',
+      data: [
         {
-          name: 'Answer',
-          type: 'pie',
-          radius: [30, 110],
-          data: [
-            { value: 33, name: 'It is blue because it is blue.' },
-            { value: 52, name: 'Many science, such wow, very fascinate.', },
-            { value: 14, name: 'What really _is_ blue?' },
-          ]
+          name: 'left',
+          value: 1
+        },
+        {
+          name: 'counter-left',
+          value: 0
         }
       ]
-    },
-    {
-      title: {
-        text: 'Question-01',
-        subtext: 'Mocking Data',
-        x: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      calculable: true,
-      series: [
-        {
-          name: 'Answer',
-          type: 'pie',
-          radius: [30, 110],
-          data: [
-            { value: 33, name: 'It is blue because it is blue.' },
-            { value: 52, name: 'Many science, such wow, very fascinate.', },
-            { value: 14, name: 'What really _is_ blue?' },
-          ]
-        }
-      ]
-    },
-    {
-      title: {
-        text: 'Question-01',
-        subtext: 'Mocking Data',
-        x: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      calculable: true,
-      series: [
-        {
-          name: 'Answer',
-          type: 'pie',
-          radius: [30, 110],
-          data: [
-            { value: 33, name: 'It is blue because it is blue.' },
-            { value: 52, name: 'Many science, such wow, very fascinate.', },
-            { value: 14, name: 'What really _is_ blue?' },
-          ]
-        }
-      ]
-    },
-    {
-      title: {
-        text: 'Question-01',
-        subtext: 'Mocking Data',
-        x: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      calculable: true,
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
-      }]
-    }
+    })
   ];
 
 
@@ -119,53 +40,69 @@ export class AnalyticsListComponent implements OnInit {
     });
   }
 
+  questionTypeToString(questionType: QuestionTypeEnum) {
+    switch (questionType) {
+      case QuestionTypeEnum.MULTIPLE_CHOICE:
+        return 'Multiple Choice';
+      case QuestionTypeEnum.INPUT:
+        return 'User Input';
+      default:
+        return '';
+    }
+  }
+
 
   chartGenerator(question) {
     if (question.questionType === QuestionTypeEnum.MULTIPLE_CHOICE) {
+      console.log('Generating from', JSON.stringify(question))
       console.log('Generating multiple choice chart');
       return {
-        title: {
-          text: question.question,
-          x: 'center'
-        },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        calculable: true,
-        series: [
-          {
-            name: 'Answer',
-            type: 'pie',
-            radius: [30, 110],
-            data: question.data
-          }
-        ]
+        question: question.question,
+        questionType: question.questionType,
+        options: {
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          calculable: true,
+          series: [
+            {
+              name: 'Answer',
+              type: 'pie',
+              radius: [30, 110],
+              data: question.data
+            }
+          ]
+        }
+
       };
     } else if (question.questionType === QuestionTypeEnum.INPUT) {
       console.log('Generating an INPUT chart');
-      console.log('input?', question.data.map(d =>  d.name ) );
       return {
-        title: {
-          text: question.quesiton,
-          x: 'center'
-        },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        calculable: true,
-        xAxis: {
-          type: 'category',
-          data: question.data.map(d =>  d.name )
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: question.data.map(d =>  d.value ),
-          type: 'bar'
-        }]
+        question: question.question,
+        questionType: question.questionType,
+        options: {
+          title: {
+            text: question.question,
+            x: 'center'
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          calculable: true,
+          xAxis: {
+            type: 'category',
+            data: question.data.map(d => d.name)
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: question.data.map(d => d.value),
+            type: 'bar'
+          }]
+        }
       };
     }
   }
